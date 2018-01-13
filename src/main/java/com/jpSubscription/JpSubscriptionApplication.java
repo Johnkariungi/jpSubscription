ec70a23ef269e07c434506378b387b95f6f17b39
John Kariungi <john.k.gitonga@gmail.com>
2018-01-12 19:20:24 -0800

    Initial commit for our Spring boot project

---
+++ src/main/java/com/jpSubscription/JpSubscriptionApplication.java
@@ -0,0 +1,12 @@
+package com.jpSubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpSubscriptionApplication.class, args);
+	}
+}
